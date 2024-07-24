2e8db53c961f4a329a89a52555b99e0ac58ce47b
Nikhil Venkat Sai Reddy Byreddy <byreddynikhil29@gmail.com>
2024-07-23 23:44:26 -0500

    go to top arrow

--- script.js
+++ script.js
@@ -8,38 +8,38 @@ function toggleMenu() {
 function scrollToTop() {
   window.scrollTo({ top: 0, behavior: 'smooth' });
 }
-const nodemailer = require('nodemailer');
+// const nodemailer = require('nodemailer');
 
-exports.handler = async function(event, context) {
-  const { name, email, message } = JSON.parse(event.body);
+// exports.handler = async function(event, context) {
+//   const { name, email, message } = JSON.parse(event.body);
 
-  const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: 'byreddynikhil29@gmail.com',
-      pass: '#Sreenath2971',
-    },
-  });
+//   const transporter = nodemailer.createTransport({
+//     service: 'gmail',
+//     auth: {
+//       user: 'byreddynikhil29@gmail.com',
+//       pass: '#Sreenath2971',
+//     },
+//   });
 
-  const mailOptions = {
-    from: email,
-    to: 'byreddynikhil29@gmail.com',
-    subject: `Contact Form Submission from ${name}`,
-    text: message,
-  };
+//   const mailOptions = {
+//     from: email,
+//     to: 'byreddynikhil29@gmail.com',
+//     subject: `Contact Form Submission from ${name}`,
+//     text: message,
+//   };
 
-  try {
-    await transporter.sendMail(mailOptions);
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ message: 'Message sent successfully!' }),
-    };
-  } catch (error) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error: error.toString() }),
-    };
-  }
-};
+//   try {
+//     await transporter.sendMail(mailOptions);
+//     return {
+//       statusCode: 200,
+//       body: JSON.stringify({ message: 'Message sent successfully!' }),
+//     };
+//   } catch (error) {
+//     return {
+//       statusCode: 500,
+//       body: JSON.stringify({ error: error.toString() }),
+//     };
+//   }
+// };
 
 
